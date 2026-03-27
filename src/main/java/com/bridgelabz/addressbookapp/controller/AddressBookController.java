469a8f4ec28eb54b0a722b7b04d9fed930b1c6a3
Sneha <snehangap@gmail.com>
2026-03-27 13:51:11 +0530

    [Sneha]: Added: Section 2 UC2: Service Layer with List Storage

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,21 +1,29 @@
 package com.bridgelabz.addressbookapp.controller;
 
+import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
+import com.bridgelabz.addressbookapp.model.AddressBook;
+import com.bridgelabz.addressbookapp.service.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressBookController {
 
+    @Autowired
+    private AddressBookService service;
+
+    // POST
+    @PostMapping("/create")
+    public AddressBook create(@RequestBody AddressBookDTO dto) {
+        return service.addAddress(dto);
     }
 
-    @GetMapping("/{id}")
-    }
-
-    @PutMapping("/{id}")
-    }
-
-    // DELETE
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable int id) {
+    // GET ALL
+    @GetMapping("/all")
+    public List<AddressBook> getAll() {
+        return service.getAllAddresses();
     }
 }
